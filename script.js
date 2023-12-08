86088c34be5272df04a48f7e3831bd028a0767bd
earl.exe <113832556+earlexe@users.noreply.github.com>
2023-12-08 18:50:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "mwamwa";
+  question.innerHTML = "I LIKE YOU TOO!";
   gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
 
